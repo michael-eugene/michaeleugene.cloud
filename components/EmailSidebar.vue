6e490b70155c219f1cb09197d8b6dc0b97f42907
michael-eugene <eugenemichael21@yahoo.com>
2024-11-22 06:01:20 -0500

    update

--- components/EmailSidebar.vue
+++ components/EmailSidebar.vue
@@ -4,7 +4,7 @@
       class="flex flex-col items-center space-y-6 after:content-[''] after:w-px after:h-24 after:bg-slate"
     >
       <a
-        href="mailto:michael.eugene@example.com"
+        href="mailto:email@michaeleugene.cloud"
         class="vertical-text font-mono text-slate hover:text-green hover:-translate-y-1 transition-all"
       >
         email@michaeleugene.cloud
